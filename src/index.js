d6ef138dcf08a1d4cc4845ecf0ebdb643527e5a3
LRWF <Lechnolocy@foxmail.com>
2025-02-21 22:23:09 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker-mirrors." + CUSTOM_DOMAIN]: dockerHub,
+  ["quay-mirrors." + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr-mirrors." + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr-mirrors." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s-mirrors." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr-mirrors." + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith-mirrors." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr-mirrors." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging-mirrors." + CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
